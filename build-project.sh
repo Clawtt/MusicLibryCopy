4233a03615741b645967261480023c4c70aec3f3
Clawtt <k.banasik04@gmail.com>
2024-02-29 20:55:37 +0100

    c

--- build-project.sh
+++ build-project.sh
@@ -1,4 +1,4 @@
-docker build -t taco
-docker stop taco || true
-docker rm taco || true
-docke run -d -p 8080:8080 --name taco taco
+docker build -t MusicLibryCopy .
+docker stop MusicLibryCopy || true
+docker rm MusicLibryCopy || true
+docke run -d -p 8080:8080 --name MusicLibryCopyMusicLibryCopy
